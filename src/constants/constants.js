5401d9340a15cca2d623b060f2edffdfe1b740b8
Santiago Trabucco <santiagotrabucco@gmail.com>
2022-07-26 15:15:43 -0300

    Added text to constants

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -3,7 +3,7 @@ export const projects = [
     title: 'CoDebug',
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras tristique ornare dictum. Nulla elit erat, cursus eu ornare eget, fermentum sed dolor. Cras imperdiet metus vel mollis elementum. Fusce laoreet ante a pellentesque hendrerit. Nulla et orci vel sapien finibus tempor ut quis erat. Duis accumsan libero tellus, hendrerit porta leo mollis at.",
       image: '/images/codebug.png',
-      tags: ['Redux', 'Express', 'React', 'Node'],
+      tags: ['Redux', 'Express', 'React', 'Node.js'],
     source: 'https://github.com/Codebug-Henry/Co-debug',
     visit: 'https://codebug-ten.vercel.app/',
     id: 0,
