59a65cc1935a014e71274c23c8e9c3a094c24bad
Santiago Trabucco <santiagotrabucco@gmail.com>
2022-08-03 17:43:34 -0300

    Added projects images and updated constants

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -10,7 +10,7 @@ export const projects = [
   },
   {
     title: 'Dogs API',
-    description:"Design and develop an App, getting the info from an API of dogs. This SPA includes: searches, filtering, ordering and creation.",
+    description: "Design and develop an App, getting the info from an API of dogs. This SPA includes: searches, filtering, ordering and creation.",
     image: '/images/dogs.png',
     tags: ['React', 'Redux', 'Express', 'Node.js'],
     source: 'https://github.com/Sandotcom/PI-Dogs',
@@ -19,11 +19,27 @@ export const projects = [
   },
   {
     title: 'Portfolio',
-    description:"This site was built with Next.js to learn about it",
+    description: "This site was built with Next.js to learn about it",
     image: '/images/portfolio.png',
     tags: ['React', 'Next.js', 'Styled Components'],
     source: 'https://github.com/Sandotcom/portfolio',
     visit: 'https://sandotcom.vercel.app/',
     id: 2,
+  },
+  {
+    title: 'YouTube Backend clone',
+    description: "The purpuse of this project is try to recreate the YouTube Back-end structure.",
+    image: '/images/youtube2.png',
+    tags: ['Node.js', 'Express', 'MongoDB', 'JWT'],
+    source: 'https://github.com/Sandotcom/YouTube-Clone',
+    id: 3,
+  },
+  {
+    title: 'Auth project',
+    description: "Setup of a Backend structure that allows users to registrer or sign in to your project. It was developed with JWT and Google OAuth",
+    image: '/images/auth.png',
+    tags: ['Node.js', 'Express', 'MongoDB', 'JWT', 'Google OAuth'],
+    source: 'https://github.com/Sandotcom/Auth',
+    id: 4,
   },
 ];
