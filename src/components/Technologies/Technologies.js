4bca293944572b0a221fc79892664725aca5128f
Santiago Trabucco <santiagotrabucco@gmail.com>
2022-11-04 14:53:06 -0300

    Add CV and technologies

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -64,9 +64,10 @@ const Technologies = ({ inSpanish }) => {
             <ListParagraph>
               Experience with <br />
               React.js <br />
+              Next.js <br />
+              React Query <br />
               Redux <br />
-              CSS <br />
-              Material UI
+              Tailwind CSS
             </ListParagraph>
           </ListContainer>
         </ListItem>
